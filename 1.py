e384f6a183e74124b95d02ef4b0af4d1697d3b54
Ashot <ashot.movsisyan.97@gmail.com>
2025-10-15 12:05:16 +0400

    class

--- 1.py
+++ 1.py
@@ -1,29 +1,41 @@
 import pygame
-import math 
+import random
+import math
 
 pygame.init()
 
 screen_width = 1200
 screen_height = 700
 width, height = 50, 50
+n = 10
+g = 0.5
 
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-v_x, v_y, w = -9, -9, 0
-g=0.5
-x, y = screen_width-width / 2, screen_height-height / 2
+# v_x, v_y, w = -9, -9, 0
+# v_x2, v_y2, w2 = -12, -15, 0
+# x, y = screen_width-width / 2, screen_height-height / 2
+x = []
+y = []
+v_x = []
+v_y = []
+w = 0
+angle = []
+
 # x, y = 0, 0
 # x, y = width / 2, height / 2
-angle = 0  # degrees
+# angle = 0  # degrees
+# angle2 = 0  # degrees
 
 # Step 1: Create a transparent surface
-rect_surf = pygame.Surface((width, height), pygame.SRCALPHA) # chatgpt
+rect_surf = pygame.Surface((width, height), pygame.SRCALPHA)  # chatgpt
 pygame.draw.rect(rect_surf, (0, 128, 255), (0, 0, width, height))
 
 screen.fill((255, 255, 255))
 
-def draw(x,y,angle):
+
+def draw(x, y, angle):
 
     # Step 2: Rotate the surface
 
@@ -33,22 +45,21 @@ def draw(x,y,angle):
     rotated_rect = rotated_surf.get_rect(center=(x, y))
 
     # Step 4: Blit to screen
-    # screen.fill((255, 255, 255))
     screen.blit(rotated_surf, rotated_rect.topleft)
 
     pygame.display.flip()
-    clock.tick(30)  # 60 FPS
 
-def move(x,y,angle):
-    global v_y, v_x
+
+def move(x, v_x, y, v_y, angle):
     x += v_x
     y += v_y
 
     v_y += g
     # v_x +=g
-    angle += w  
+    angle += w
 
-    return clash(x,y,angle)
+    return clash(x, v_x, y, v_y, angle)
+
 
 def sign(x):
     if x == 0:
@@ -57,36 +68,50 @@ def sign(x):
         return -1
     return 1
 
-def clash(x,y,angle):
-    global v_x
-    global v_y
+
+def clash(x, v_x, y, v_y, angle):
     delta = 0
 
     if x + width / 2 >= screen_width or x < width / 2:
         v_x = -v_x
 
     if y + height / 2 >= screen_height or y < height / 2:
-        
-        if y > screen_height-height / 2:
+
+        if y > screen_height - height / 2:
             delta = 2 * (y - screen_height + height / 2)
-            v_y = - sign(v_y) * math.sqrt(v_y ** 2 - sign(v_y) * 2 * delta * g)
-            y = 2 * (screen_height-height/2) - y
+            v_y = -sign(v_y) * math.sqrt(v_y**2 - sign(v_y) * 2 * delta * g)
+            # y = 2 * (screen_height-height/2) - y
+            y = screen_height - height / 2 - delta
 
         if y < height / 2:
             delta = height - 2 * y
-            v_y = - sign(v_y) * math.sqrt(v_y ** 2 - sign(v_y) * 2 * delta * g)
+            v_y = -sign(v_y) * math.sqrt(v_y**2 - sign(v_y) * 2 * delta * g)
             y = height - y
 
-        
+    return x, v_x, y, v_y, angle
 
-    
-    return x,y,angle
+
+
+for index in range(n):
+    x.insert(index, random.uniform(0 + width / 2, screen_width - width / 2))
+    y.insert(index, random.uniform(0 + height / 2, screen_height - height / 2))
+    v_x.insert(index, random.uniform(-15, 15))
+    v_y.insert(index, random.uniform(-15, 15))
+    angle.insert(index, 0)
+
+# print('x', x)
+# print('y', y)
+# print('v_x', v_x)
+# print('v_y', v_y)
 
 for i in range(1000):
-    x, y, angle = move(x,y,angle)
+    screen.fill((255, 255, 255))
 
-    draw(x,y,angle)
+    for index in range(n):
+        x[index], v_x[index], y[index], v_y[index], angle[index] = move(x[index], v_x[index], y[index], v_y[index], angle[index])
+        draw(x[index], y[index], angle[index])
+
+    clock.tick(30)  # 60 FPS
+
 
 pygame.quit()
-
-
---
+++ class-square.py
@@ -0,0 +1,103 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+screen_width = 1200
+screen_height = 700
+width, height = 5, 5
+n = 10
+g = 0.5
+w = 0
+
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+rect_surf = pygame.Surface((width, height), pygame.SRCALPHA)  # chatgpt
+pygame.draw.rect(rect_surf, (0, 128, 255), (0, 0, width, height))
+
+def sign(x):
+    if x == 0:
+        return 0
+    if x < 0:
+        return -1
+    return 1
+
+class Square:
+    def __init__(self, x, y, v_x, v_y, angle):
+        self.x = x
+        self.y = y
+        self.v_x = v_x
+        self.v_y = v_y
+        self.angle = angle
+
+    def print(self):
+        print(f"({self.x}, {self.y})")
+
+    def move(self):
+        self.x += self.v_x
+        self.y += self.v_y + g / 2
+
+        self.v_y += g
+        # v_x +=g
+        self.angle += w
+
+        delta = 0
+
+        if self.x + width / 2 >= screen_width or self.x < width / 2:
+            self.v_x = -self.v_x
+
+        if self.y + height / 2 >= screen_height or self.y < height / 2:
+
+            if self.y > screen_height - height / 2:
+                delta = 2 * (self.y - screen_height + height / 2)
+                self.v_y = -sign(self.v_y) * math.sqrt(self.v_y**2 - sign(self.v_y) * 2 * delta * g)
+                # y = 2 * (screen_height-height/2) - y
+                self.y = screen_height - height / 2 - delta
+
+            if self.y < height / 2:
+                delta = height - 2 * self.y
+                self.v_y = -sign(self.v_y) * math.sqrt(self.v_y**2 - sign(self.v_y) * 2 * delta * g)
+                self.y = height - self.y
+
+        
+
+    def draw(self):
+        rotated_surf = pygame.transform.rotate(rect_surf, self.angle)
+        rotated_rect = rotated_surf.get_rect(center=(self.x, self.y))
+        screen.blit(rotated_surf, rotated_rect.topleft)
+        pygame.display.flip()
+
+
+squares = []
+
+
+for i in range(n):
+    squares.insert(
+        n,
+        Square(
+            random.uniform(0 + width / 2, screen_width - width / 2),
+            random.uniform(0 + height / 2, screen_height - height / 2),
+            random.uniform(-15, 15),
+            random.uniform(-15, 15),
+            0,
+        ),
+    )
+
+
+
+
+screen.fill((255, 255, 255))
+
+for i in range(1000):
+
+    for i in range(n):
+        squares[i].move()
+        squares[i].draw()
+
+    clock.tick(30)
+
+
+pygame.quit()
